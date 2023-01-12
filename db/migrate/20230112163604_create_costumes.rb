aa9fb5ff860a48f922fce9109750f0f8266a82b3
Brooke <bjones782@gmail.com>
2023-01-12 11:44:23 -0500

    commit tree 7bd113eb9e693ea6c10f6e1bf07e43816f2716cb

---
+++ db/migrate/20230112163604_create_costumes.rb
@@ -0,0 +1,10 @@
+class CreateCostumes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+    end
+  end
+end
